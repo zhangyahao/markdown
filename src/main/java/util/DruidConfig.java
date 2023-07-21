b6ef8600b0af2aec4edee644c9ac8b216fe980a8
yinShang <810720094@qq.com>
2023-07-21 09:59:44 +0800

    commit

---
+++ src/main/java/util/DruidConfig.java
@@ -0,0 +1,22 @@
+package util;
+import com.alibaba.druid.filter.config.ConfigTools;
+/**
+ * Druid
+ */
+public class DruidConfig {
+    public static void main(String[] args) throws Exception{
+        String[] arr = genKeyPair(521);
+
+        String privateKey = arr[0];
+        System.out.println("privateKey:+"+privateKey);
+
+        String publicKey = arr[1];
+        System.out.println("publicKey:+"+publicKey);
+
+        String pass = "rckp@ssw0rd";
+        String cipherTex = encrypt(privateKey, pass);
+        System.out.println("加密结果+"+cipherTex);
+
+        System.out.println("解密结果："+decrypt(publicKey,cipherTex));
+    }
+}
